2ce78b5d987a56e72122f1ac28ebc0241560bbdb
LeoGomes0919 <leonardogs0919@gmail.com>
2020-10-02 17:57:18 -0300

    Deploy Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,16 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'balarama.db.elephantsql.com',
-  username: 'kpmscsoh',
-  password: 'gnXc49STGiXCuzbVLrZtkp96E2qNDrLF',
-  database: 'kpmscsoh',
+  host: 'ec2-18-211-86-133.compute-1.amazonaws.com',
+  username: 'fubaapjorqhcph',
+  password: 'bdb52a8a0ad4f36712c10de4385bda4fb9eeda8f03af4b8a20445c24ee31c1c3',
+  database: 'ddasqhqv0b8092',
+  port: 5432,
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true
+    }
+  },
   define: {
     timestamps: true,
     underscored: true,
