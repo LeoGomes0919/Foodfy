65ae77e0ee8b7bfb5b6004533002a420237cdd2a
Leonardo Gomes <leonardogs0919@gmail.com>
2020-10-02 17:10:19 -0300

    Update seeders de users.

--- src/database/seeders/20200926203836-user-seeder.js
+++ src/database/seeders/20200926203836-user-seeder.js
@@ -6,7 +6,7 @@ module.exports = {
     const passwordHas = hashSync('1234', 8);
     await queryInterface.bulkInsert('users', [{
       name: faker.name.firstName(),
-      email: faker.internet.email(),
+      email: 'admin@foodfy.com.br',
       password: passwordHas,
       reset_token: '',
       reset_token_expires: '',
