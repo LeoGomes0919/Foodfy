c4b11063a0c520196ee7238e5937c6455b464866
LeoGomes0919 <leonardogs0919@gmail.com>
2020-10-02 19:39:18 -0300

    Deploy update auth.

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,15 @@
 module.exports = {
   dialect: 'postgres',
   host: 'ec2-54-156-121-142.compute-1.amazonaws.com',
+  database: 'des647hpi31vq8',
   username: 'edongdognooiif',
   password: '15b86731cac72c7e10d441869437ed078841e05e8bad830ce24980cfbab9a99a',
-  database: 'des647hpi31vq8',
   port: 5432,
   ssl: true,
   dialectOptions: {
     ssl: {
-      require: true
+      require: true,
+      rejectUnauthorized: false
     }
   },
   define: {
