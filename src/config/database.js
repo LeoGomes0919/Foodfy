a6bedd949479e129fd0cd2d7915f92c87bf927f3
Leonardo Gomes <leonardogs0919@gmail.com>
2020-10-02 22:59:32 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,10 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'ec2-54-156-121-142.compute-1.amazonaws.com',
-  database: 'des647hpi31vq8',
-  username: 'edongdognooiif',
-  password: '15b86731cac72c7e10d441869437ed078841e05e8bad830ce24980cfbab9a99a',
+  host: 'localhost',
+  database: 'Foodfy',
+  username: 'postgres',
+  password: '1234',
   port: 5432,
-  ssl: true,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
   define: {
     timestamps: true,
     underscored: true,
